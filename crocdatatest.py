db45fd06001493a9e6000048eb199cd3780daf73
JJ-byte1 <114854373+JJ-byte1@users.noreply.github.com>
2022-11-11 10:35:03 +0930

    Added Part A next_site()

--- crocdatatest.py
+++ crocdatatest.py
@@ -26,6 +26,7 @@ class monitor():
     def __init__(self, size):
         self.distance_data = []
         self.list_of_locations = []
+        self.locations = []
         self.edges_data = []
         self.num_of_sightings= []
         self.matrix = [[0 for x in range(size)]for y in range(size)]
@@ -49,6 +50,7 @@ class monitor():
                             gdist= line[7]
                             
                             self.list_of_locations .append([nodenum, x, y, env, numsight, edgea, water, gdist])
+                            self.locations.append([x,y])
                             self.edges_data .append([nodenum, edgea])
                             self.distance_data .append([nodenum,edgea,gdist])
                             if line[0] != '':
@@ -80,6 +82,45 @@ def Bubble_Sort(unsorted_list):
 cm=monitor(size)
 unsorted_list = cm.num_of_sightings
 
+
+'''
+Assumptions:
+    A radius of 2 km (!! Not sure if km is correct unit for the map!!) was assumed to be 
+    within the range of nearby to the researchers. 
+    
+    This assumption also means nodes who's direct travel path passes through water will also 
+    be considered.
+    
+    The argument 'previous_number' did not need to be utilised within the method.
+'''
+def next_site(previous_site, previous_number):
+    radius = 2 # you can configure radius to change scope of search for nearby
+    sightings = cm.num_of_sightings
+    locations = cm.locations
+    x_previous_site, y_previous_site = locations[previous_site-1][0], locations[previous_site-1][1] # storing location of previous site
+    
+    count = 0
+    max = 0
+    max_location = []
+    
+    for loc in locations: # iterating through all map locations
+        x_current_site = loc[0]
+        y_current_site = loc[1]
+
+        if abs(x_previous_site - x_current_site) <= radius and abs(y_previous_site - y_current_site) <= radius: # checking if current location is within radius
+            if max < sightings[count]: # update max if current location has largest sight num
+                max = sightings[count]
+                max_location = locations[count]
+                max_site = count + 1
+
+            elif max == sightings[count]: # update max location to location with equal max sight num and lower distance to previous_number 
+                if distance(previous_site, max_site) < distance(previous_site, i + 1):
+                    max_location = locations[count]
+                    max_site = count + 1
+
+        count += 1
+
+    return ([max_location, max_site])
 
 # #bubblesort in ascendind order
 # def Bubble_Sort(unsorted_list):
